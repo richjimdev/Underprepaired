22620136d60cce8e99765edec41833dd22f4dc05
Richard J <richjim89@gmail.com>
2018-11-14 15:42:45 -0800

    Seed works on my machine

---
+++ Underprepaired/Underprepaired/Migrations/20181114233215_testRichard.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Underprepaired.Migrations
+{
+    public partial class testRichard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
