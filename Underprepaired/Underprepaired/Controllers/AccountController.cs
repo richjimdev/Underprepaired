027255860685b7f34062a2883e75a26bb8621eba
Richard J <richjim89@gmail.com>
2018-11-14 12:53:25 -0800

    created account controller

---
+++ Underprepaired/Underprepaired/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Underprepaired.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
