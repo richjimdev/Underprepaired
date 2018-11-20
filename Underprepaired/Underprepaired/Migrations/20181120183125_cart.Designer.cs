657e72322d6efc5e77223d7f650bd8d41af7a983
Aaron Frank <aaron.frank41@gmail.com>
2018-11-20 10:33:25 -0800

    added carts tables

--- Underprepaired/Underprepaired/Migrations/20181120183125_cart.Designer.cs
+++ Underprepaired/Underprepaired/Migrations/20181120183125_cart.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Underprepaired.Data;
 
 namespace Underprepaired.Migrations
 {
     [DbContext(typeof(UnderprepairedDbContext))]
-    partial class UnderprepairedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181120183125_cart")]
+    partial class cart
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Underprepaired/Underprepaired/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Underprepaired.Models
+{
+    public class Cart
+    {
+        // Primary Key
+        public int ID { get; set; }
+        
+        public int UserID { get; set; }
+
+        // Nav Property
+        public ICollection<CartItem> CartItem { get; set; }
+    }
+}
