fa21ddec90376922823cbbd2efca54339c9a07cc
Richard J <richjim89@gmail.com>
2018-11-28 12:21:43 -0800

    more stuff

--- Underprepaired/Underprepaired/wwwroot/js/app.js
+++ Underprepaired/Underprepaired/wwwroot/js/app.js
@@ -34,6 +34,15 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
+
+    $('.dropdown').on('show.bs.dropdown', function (e) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    });
+
+    $('.dropdown').on('hide.bs.dropdown', function (e) {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+    });
+
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
