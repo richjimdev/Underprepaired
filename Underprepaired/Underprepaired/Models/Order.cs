7571786e46addac3ced32a9eddc3e7ab5f8747fa
Aaron Frank <aaron.frank41@gmail.com>
2018-12-05 10:51:58 -0800

    created order table

---
+++ Underprepaired/Underprepaired/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Underprepaired.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
